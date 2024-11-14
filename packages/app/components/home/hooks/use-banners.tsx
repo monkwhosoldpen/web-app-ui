e1fcaeef4c8814f5b59c4446eac18a1b8800b2f2
monkwhosoldpen <monkwhosoldpen@gmail.com>
2024-11-15 05:02:55 +0530

    Update use-banners.tsx

--- packages/app/components/home/hooks/use-banners.tsx
+++ packages/app/components/home/hooks/use-banners.tsx
@@ -20,38 +20,37 @@ export const useBanners = () => {
   const mockData: Banner[] = [
     {
       type: "profile",
-      username: "JohnDoe",
+      username: "elonmusk",
       slug: "",
       link: "https://example.com",
-      image: "https://via.placeholder.com/300/FF0000/FFFFFF?text=Banner1",
+      image: "https://picsum.photos/seed/picsum/5000/3333",
     },
     {
       type: "profile",
-      username: "JohnDoe",
+      username: "elonmusk",
       slug: "",
       link: "https://example.com",
-      image: "https://via.placeholder.com/300/00FF00/FFFFFF?text=Banner2",
+      image: "https://picsum.photos/id/870/5000/3333",
     },
     {
       type: "profile",
-      username: "JohnDoe",
+      username: "elonmusk",
       slug: "",
       link: "https://example.com",
-      image: "https://via.placeholder.com/300/0000FF/FFFFFF?text=Banner3",
+      image: "https://picsum.photos/id/522/5000/3333",
     },
     {
       type: "profile",
-      username: "JohnDoe",
+      username: "elonmusk",
       slug: "",
       link: "https://example.com",
-      image: "https://via.placeholder.com/300/FFFF00/FFFFFF?text=Banner4",
+      image: "https://picsum.photos/id/3/5000/3333",
     },
-    // Add more pleasant events with corresponding Wikipedia or reliable sources links
   ];
 
   return {
     data: mockData,
-    isLoading: false
+    isLoading: false,
     // Mock rest of the properties or methods if required
   };
 };
